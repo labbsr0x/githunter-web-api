e87bf4c17dbc653efe551dd199583018e7441839
Ramon Ferreira <ramonfsk@gmail.com>
2020-10-09 17:23:58 -0300

    implemented external-service starws-bind refs #3

--- config/Config.d.ts
+++ config/Config.d.ts
@@ -3,12 +3,24 @@
 declare module "node-config-ts" {
   interface IConfig {
     server: Server
+    starws: Starws
   }
   interface Server {
     url: string
     port: number
     baseDir: string
   }
+
+  interface StarwsEndpoints {
+    metrics: string;
+  }
+
+  interface Starws {
+    host: string;
+    port: string;
+    endpoints: StarwsEndpoints
+  }
+
   export const config: Config
   export type Config = IConfig
 }
