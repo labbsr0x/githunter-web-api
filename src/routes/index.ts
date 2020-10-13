7b245ad732137705b216037656c7cf8037484ed9
Ramon Ferreira <ramonfsk@gmail.com>
2020-10-13 11:37:59 -0300

    fixed according to the suggestions closes #1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,5 @@
 import { Router } from 'express';
 
-import repositoriesRouter from './repositories.routes';
-// import services to use instance on routes
-
 const routes = Router();
-
-routes.use('/repositories', repositoriesRouter);
 
 export default routes;
--- src/routes/repositories.routes.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-
-const repositoriesRouter = Router();
-
-export default repositoriesRouter;
